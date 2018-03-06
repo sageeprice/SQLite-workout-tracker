ef1dc0728781ef95446f4d819990d75b7d7ab7a3
sageeprice <sageeprice@gmail.com>
2018-03-05 21:35:08 -0500

    Removed unused JS.

--- js/test.js
+++ js/test.js
@@ -1,79 +1,4 @@
-/*
-// TODO: send Post request with contents of sets when a submit button is hit.
-var sets = {
-  setList: [],
-
-  addSet: function(liftName, weight, reps) {
-    this.setList.push({
-      liftName: liftName,
-      weight: weight,
-      reps: reps
-    })
-  },
-
-  removeSet: function(position) {
-    this.setList.splice(position, 1);
-  }
-};
-
-var handlers = {
-  addSet: function() {
-    var liftName = document.getElementById('liftName');
-    var weight = document.getElementById('weightInput');
-    var reps = document.getElementById('repsInput');
-
-    sets.addSet(liftName.value, weight.value, reps.value);
-    
-    // Reset these fields to empty, leave lift as is.
-    weight.value = '';
-    reps.value = '';
-    
-    view.displaySets();
-  },
-  removeSet: function(position) {
-    sets.removeSet(position);
-    
-    view.displaySets();
-  }
-};
-
-var view = {
-  displaySets: function() {
-    var setsUl = document.querySelector('ul');
-    setsUl.innerHTML = '';
-
-    sets.setList.forEach(function(set, position) {
-      var setLi = document.createElement('li');
-      var setLiText = set.liftName + ": " + set.weight + ' lbs for ' + set.reps + ' reps.';
-
-      setLi.id = position;
-      setLi.textContent = setLiText;
-      setLi.appendChild(this.createDeleteButton());
-      setsUl.appendChild(setLi);
-    }, this);
-  },
-  createDeleteButton: function() {
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Remove';
-    deleteButton.className = 'deleteButton';
-    return deleteButton;
-  },
-  setUpEventListeners: function() {
-    var setUl = document.querySelector('ul');
-    setUl.addEventListener('click', function(event) {
-      var setNumber = event.target.parentNode.id;
-
-      var eventClicked = event.target;
-      if (eventClicked.className === 'deleteButton') {
-        handlers.removeSet(parseInt(eventClicked.parentNode.id));
-      }
-    });
-  }
-};
-
-view.setUpEventListeners();
-
-
+/* Keeping this function as a reminder of how to set trigger on enter.
 // Trigger the addSetButton on enter in any input field.
 function triggerOnEnter(event) {
 
@@ -85,14 +10,7 @@ function triggerOnEnter(event) {
     document.getElementById("addSetButton").click();
   }
 }
-
-// Initiate form submission when enter is clicked.
-// TODO: refactor to do this for each focusable element of the form.
-var input = document.getElementById("liftName").addEventListener("keyup", triggerOnEnter);
-var input2 = document.getElementById("weightInput").addEventListener("keyup", triggerOnEnter);
-var input3 = document.getElementById("repsInput").addEventListener("keyup", triggerOnEnter);
 */
-
 
 function addFormSet() {
   var entries = document.getElementById("exerciseEntries");
